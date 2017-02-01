180e475d99512a3fc58ed970b09e76280e4e87e6
Alden Lamp <alamp9008@gmail.com>
2017-01-31 19:29:39 -0500

    Working on Store Class

--- Store.java
+++ Store.java
@@ -1,34 +1,76 @@
+import java.util.ArrayList;
 public class Store{
 
-	Item[] stock;
+	ArrayList<Item> stock = new ArrayList<Item>();
+	ArrayList<String> itemNames = new ArrayList<String>();
+	String storeName;
 
 	public Store(){
-
+		storeName = "New Store";
 	}
 
+	public Store(String name){
+		storeName = name;
+	}
 
+	public Store(String name, ArrayList<Item> items){
+		stock = items;
+		storeName = name;
+		for(Item i : items) itemNames.add(i.getName());
+	}
 
+	public void addItem(Item newItem){
+		stock.add(newItem);
+	}
+
+	public void addNewItem(String itemName){
+		stock.add(new Item(itemName));
+	}
+
+	public void addNewItem(String itemName, int amount){
+		stock.add(new Item(itemName, amount));
+	}
+
+	public void addNewItem(String itemName, int amount, double sellPrice, double buyPrice){
+		stock.add(new Item(itemName, sellPrice, buyPrice, amount));
+	}
 
 }
 
-public class Item{
+class Item{
 
 	private double sellPrice, buyPrice;
 	private boolean inStock;
 	private int amount;
 	private String name;
 
-	public Item(String name, boolean inStock){
+	public Item(String name, double sellPrice, double buyPrice){
 		this.name = name;
-		this.inStock = inStock;
+		this.sellPrice = sellPrice;
+		this.buyPrice = buyPrice;
+	}
+
+	public Item(String name, double sellPrice, double buyPrice, int amount){
+		this.name = name;
+		this.sellPrice = sellPrice;
+		this.buyPrice = buyPrice;
+		this.amount = amount;
+		if (amount > 0) this.inStock = true;
+	}
+
+	public Item(String name, int amount){
+		this.name = name;
+		this.amount = amount;
+		if (amount > 0) this.inStock = true;
 	}
 
 	public Item(String name){
 		this.name = name;	
 	}
 
+
 	public Item(){
-		this.name = "";
+		this.name = "New Store Item";
 	}
 
 	public void addItem(int numberOfItems){
